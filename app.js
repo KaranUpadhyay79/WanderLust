5fbb1cb04ac1205d09ce305580c21849ce51936a
Karan Kumar Upadhyay <upadhyaykarankumar332@gmail.com>
2024-11-29 02:27:23 +0530

    Description of the changes

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ const listingRouter = require("./routes/listing.js");
 const reviewRouter = require ("./routes/reviews.js");
 const userRouter = require ("./routes/user.js");
 
-
+const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 const dbUrl = process.env.ATLASDB_URL;
 
 main()
@@ -41,20 +41,20 @@ app.use(methodOverride("_method"));
 app.engine('ejs' , ejsMate);
 app.use(express.static(path.join(__dirname,"/public")));
 
-const store = MongoStore.create({
-mongoUrl : dbUrl ,
-    crypto: {
-        secret:process.env.SECRET,
-    },
-    touchAfter: 24 * 3600,
-});
+// const store = MongoStore.create({
+//   mongoUrl : dbUrl,
+//     crypto: {
+//         secret:process.env.SECRET,
+//     },
+//     touchAfter: 24 * 3600,
+// });
 
-store.on("error" , () => {
-    console.log("ERROR in MONGO SESSION STORE",err);
-});
+// store.on("error" , () => {
+//     console.log("ERROR in MONGO SESSION STORE",err);
+// });
 
 const sessionOptions = {
-    store,
+    //store,
     secret:process.env.SECRET,
     resave:false,
     saveUninitialized : true,
